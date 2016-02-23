5e212373c405ebfc2b6e96683dd4c5cc99894c59
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-02-23 16:02:44 +0700

    faq: settings

---
+++ modules/faq/views/o/setting/_form.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * @var $this SettingController
+ * @var $model FaqSetting
+ * @var $form CActiveForm
+ *
+ * @author Putra Sudaryanto <putra.sudaryanto@gmail.com>
+ * @copyright Copyright (c) 2014 Ommu Platform (ommu.co)
+ * @link http://company.ommu.co
+ * @contact (+62)856-299-4114
+ *
+ */
+?>
+
+<?php $form=$this->beginWidget('application.components.system.OActiveForm', array(
+	'id'=>'faq-setting-form',
+	'enableAjaxValidation'=>true,
+	//'htmlOptions' => array('enctype' => 'multipart/form-data')
+)); ?>
+
+<?php //begin.Messages ?>
+<div id="ajax-message">
+	<?php echo $form->errorSummary($model); ?>
+</div>
+<?php //begin.Messages ?>
+
+<fieldset>
+
+	<div class="clearfix">
+		<label>
+			<?php echo $model->getAttributeLabel('license');?> <span class="required">*</span><br/>
+			<span><?php echo Phrase::trans(11003,1);?></span>
+		</label>
+		<div class="desc">
+			<?php echo $form->textField($model,'license',array('maxlength'=>32,'class'=>'span-4','disabled'=>'disabled')); ?>
+			<?php echo $form->error($model,'license'); ?>
+			<span class="small-px"><?php echo Phrase::trans(11004,1);?></span>
+		</div>
+	</div>
+
+	<div class="clearfix">
+		<?php echo $form->labelEx($model,'permission'); ?>
+		<div class="desc">
+			<span class="small-px"><?php echo Phrase::trans(11006,1);?></span>
+			<?php echo $form->radioButtonList($model, 'permission', array(
+				1 => Phrase::trans(11007,1),
+				0 => Phrase::trans(11008,1),
+			)); ?>
+			<?php echo $form->error($model,'permission'); ?>
+		</div>
+	</div>
+
+	<div class="clearfix">
+		<?php echo $form->labelEx($model,'meta_description'); ?>
+		<div class="desc">
+			<?php echo $form->textArea($model,'meta_description',array('rows'=>6, 'cols'=>50, 'class'=>'span-7 smaller')); ?>
+			<?php echo $form->error($model,'meta_description'); ?>
+		</div>
+	</div>
+
+	<div class="clearfix">
+		<?php echo $form->labelEx($model,'meta_keyword'); ?>
+		<div class="desc">
+			<?php echo $form->textArea($model,'meta_keyword',array('rows'=>6, 'cols'=>50, 'class'=>'span-7 smaller')); ?>
+			<?php echo $form->error($model,'meta_keyword'); ?>
+		</div>
+	</div>
+
+	<div class="submit clearfix">
+		<label>&nbsp;</label>
+		<div class="desc">
+			<?php echo CHtml::submitButton($model->isNewRecord ? Phrase::trans(1,0) : Phrase::trans(2,0), array('onclick' => 'setEnableSave()')); ?>
+		</div>
+	</div>
+
+</fieldset>
+<?php $this->endWidget(); ?>
---
+++ modules/faq/views/o/setting/admin_index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @var $this SettingController
+ * @var $model FaqSetting
+ *
+ * @author Putra Sudaryanto <putra.sudaryanto@gmail.com>
+ * @copyright Copyright (c) 2014 Ommu Platform (ommu.co)
+ * @link http://company.ommu.co
+ * @contact (+62)856-299-4114
+ *
+ */
+ 
+	$this->breadcrumbs=array(
+		'Faq Settings'=>array('manage'),
+		$model->id=>array('view','id'=>$model->id),
+		'Update',
+	);
+?>
+
+<div class="form" name="post-on">
+	<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+</div>
