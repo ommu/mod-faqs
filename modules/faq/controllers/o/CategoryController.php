b0dae9f5221309afab57e3766bef13050b7ae7bf
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-11-26 08:24:14 +0700

    change link repository

--- modules/faq/controllers/o/CategoryController.php
+++ modules/faq/controllers/o/CategoryController.php
@@ -21,7 +21,7 @@
  *
  * @author Putra Sudaryanto <putra@sudaryanto.id>
  * @copyright Copyright (c) 2012 Ommu Platform (opensource.ommu.co)
- * @link https://github.com/ommu/mod-frequency-asked-question
+ * @link https://github.com/ommu/ommu-faqs
  * @contact (+62)856-299-4114
  *
  *----------------------------------------------------------------------------------------------------------
