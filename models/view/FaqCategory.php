1a6e4498ef4c06579de7483bfd924d4d61af7020
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-04-27 00:34:31 +0700

    add model-view

---
+++ models/view/FaqCategory.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * FaqCategory
+ * 
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @contact (+62)856-299-4114
+ * @copyright Copyright (c) 2018 ECC UGM (ecc.ft.ugm.ac.id)
+ * @created date 27 April 2018, 00:32 WIB
+ * @link https://ecc.ft.ugm.ac.id
+ *
+ * This is the model class for table "_faq_category".
+ *
+ * The followings are the available columns in table "_faq_category":
+ * @property integer $cat_id
+ * @property string $faqs
+ * @property integer $faq_all
+ *
+ */
+
+namespace app\modules\faq\models\view;
+
+use Yii;
+use yii\helpers\Url;
+use yii\helpers\Html;
+
+class FaqCategory extends \app\components\ActiveRecord
+{
+	public $gridForbiddenColumn = [];
+
+	/**
+	 * @return string the associated database table name
+	 */
+	public static function tableName()
+	{
+		return '_faq_category';
+	}
+
+	/**
+	 * @return string the primarykey column
+	 */
+	public static function primaryKey()
+	{
+		return ['cat_id'];
+	}
+
+	/**
+	 * @return \yii\db\Connection the database connection used by this AR class.
+	 */
+	public static function getDb()
+	{
+		return Yii::$app->get('ecc4');
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		return [
+			[['cat_id', 'faq_all'], 'integer'],
+			[['faqs'], 'number'],
+		];
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return [
+			'cat_id' => Yii::t('app', 'Category'),
+			'faqs' => Yii::t('app', 'Faqs'),
+			'faq_all' => Yii::t('app', 'Faq All'),
+		];
+	}
+
+	/**
+	 * Set default columns to display
+	 */
+	public function init() 
+	{
+		parent::init();
+
+		$this->templateColumns['_no'] = [
+			'header' => Yii::t('app', 'No'),
+			'class'  => 'yii\grid\SerialColumn',
+			'contentOptions' => ['class'=>'center'],
+		];
+		$this->templateColumns['cat_id'] = [
+			'attribute' => 'cat_id',
+			'value' => function($model, $key, $index, $column) {
+				return $model->cat_id;
+			},
+		];
+		$this->templateColumns['faqs'] = [
+			'attribute' => 'faqs',
+			'value' => function($model, $key, $index, $column) {
+				return $model->faqs;
+			},
+		];
+		$this->templateColumns['faq_all'] = [
+			'attribute' => 'faq_all',
+			'value' => function($model, $key, $index, $column) {
+				return $model->faq_all;
+			},
+		];
+	}
+
+	/**
+	 * User get information
+	 */
+	public static function getInfo($id, $column=null)
+	{
+		if($column != null) {
+			$model = self::find()
+				->select([$column])
+				->where(['cat_id' => $id])
+				->one();
+			return $model->$column;
+			
+		} else {
+			$model = self::findOne($id);
+			return $model;
+		}
+	}
+}
---
+++ models/view/Faqs.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ * Faqs
+ * 
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @contact (+62)856-299-4114
+ * @copyright Copyright (c) 2018 ECC UGM (ecc.ft.ugm.ac.id)
+ * @created date 27 April 2018, 00:33 WIB
+ * @link https://ecc.ft.ugm.ac.id
+ *
+ * This is the model class for table "_faqs".
+ *
+ * The followings are the available columns in table "_faqs":
+ * @property integer $faq_id
+ * @property integer $helpfuls
+ * @property string $views
+ * @property string $view_all
+ * @property string $likes
+ * @property integer $like_all
+ *
+ */
+
+namespace app\modules\faq\models\view;
+
+use Yii;
+use yii\helpers\Url;
+use yii\helpers\Html;
+
+class Faqs extends \app\components\ActiveRecord
+{
+	public $gridForbiddenColumn = [];
+
+	/**
+	 * @return string the associated database table name
+	 */
+	public static function tableName()
+	{
+		return '_faqs';
+	}
+
+	/**
+	 * @return string the primarykey column
+	 */
+	public static function primaryKey()
+	{
+		return ['faq_id'];
+	}
+
+	/**
+	 * @return \yii\db\Connection the database connection used by this AR class.
+	 */
+	public static function getDb()
+	{
+		return Yii::$app->get('ecc4');
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		return [
+			[['faq_id', 'helpfuls', 'like_all'], 'integer'],
+			[['views', 'view_all', 'likes'], 'number'],
+		];
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return [
+			'faq_id' => Yii::t('app', 'Faq'),
+			'helpfuls' => Yii::t('app', 'Helpfuls'),
+			'views' => Yii::t('app', 'Views'),
+			'view_all' => Yii::t('app', 'View All'),
+			'likes' => Yii::t('app', 'Likes'),
+			'like_all' => Yii::t('app', 'Like All'),
+		];
+	}
+
+	/**
+	 * Set default columns to display
+	 */
+	public function init() 
+	{
+		parent::init();
+
+		$this->templateColumns['_no'] = [
+			'header' => Yii::t('app', 'No'),
+			'class'  => 'yii\grid\SerialColumn',
+			'contentOptions' => ['class'=>'center'],
+		];
+		$this->templateColumns['faq_id'] = [
+			'attribute' => 'faq_id',
+			'value' => function($model, $key, $index, $column) {
+				return $model->faq_id;
+			},
+		];
+		$this->templateColumns['helpfuls'] = [
+			'attribute' => 'helpfuls',
+			'value' => function($model, $key, $index, $column) {
+				return $model->helpfuls;
+			},
+		];
+		$this->templateColumns['views'] = [
+			'attribute' => 'views',
+			'value' => function($model, $key, $index, $column) {
+				return $model->views;
+			},
+		];
+		$this->templateColumns['view_all'] = [
+			'attribute' => 'view_all',
+			'value' => function($model, $key, $index, $column) {
+				return $model->view_all;
+			},
+		];
+		$this->templateColumns['likes'] = [
+			'attribute' => 'likes',
+			'value' => function($model, $key, $index, $column) {
+				return $model->likes;
+			},
+		];
+		$this->templateColumns['like_all'] = [
+			'attribute' => 'like_all',
+			'value' => function($model, $key, $index, $column) {
+				return $model->like_all;
+			},
+		];
+	}
+
+	/**
+	 * User get information
+	 */
+	public static function getInfo($id, $column=null)
+	{
+		if($column != null) {
+			$model = self::find()
+				->select([$column])
+				->where(['faq_id' => $id])
+				->one();
+			return $model->$column;
+			
+		} else {
+			$model = self::findOne($id);
+			return $model;
+		}
+	}
+}
