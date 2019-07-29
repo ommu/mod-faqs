64da8773aaf2126cf86ebcf7c42ced2ba38aaee5
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-07-29 18:10:50 +0700

    modify getInfo with array column

--- models/FaqLikes.php
+++ models/FaqLikes.php
@@ -202,11 +202,13 @@ class FaqLikes extends \app\components\ActiveRecord
 	public static function getInfo($id, $column=null)
 	{
 		if($column != null) {
-			$model = self::find()
-				->select([$column])
-				->where(['like_id' => $id])
-				->one();
-			return $model->$column;
+			$model = self::find();
+			if(is_array($column))
+				$model->select($column);
+			else
+				$model->select([$column]);
+			$model = $model->where(['like_id' => $id])->one();
+			return is_array($column) ? $model : $model->$column;
 			
 		} else {
 			$model = self::findOne($id);
--- models/Faqs.php
+++ models/Faqs.php
@@ -350,11 +350,13 @@ class Faqs extends \app\components\ActiveRecord
 	public static function getInfo($id, $column=null)
 	{
 		if($column != null) {
-			$model = self::find()
-				->select([$column])
-				->where(['faq_id' => $id])
-				->one();
-			return $model->$column;
+			$model = self::find();
+			if(is_array($column))
+				$model->select($column);
+			else
+				$model->select([$column]);
+			$model = $model->where(['faq_id' => $id])->one();
+			return is_array($column) ? $model : $model->$column;
 			
 		} else {
 			$model = self::findOne($id);
